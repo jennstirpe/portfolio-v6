799c963442c777077725130ba43d2a945407aeab
jstirpe <jennstirpe@yahoo.com>
2023-04-24 16:32:29 -0400

    Added details to About module

--- src/components/modals/AboutModal.jsx
+++ src/components/modals/AboutModal.jsx
@@ -1,5 +1,6 @@
 import CloseBtn from "../global/CloseBtn"
 import { StyledAboutModal } from "./styled/AboutModal.styled"
+import resume from "../../resume.pdf";
 
 export default function AboutModal({ handleClose }) {
 
@@ -11,7 +12,54 @@ export default function AboutModal({ handleClose }) {
     <StyledAboutModal className="modal">
         <CloseBtn closeModal={closeAbout} modal="Close about modal" />
         <h3 className="modal-header">About me</h3>
-        <p className="about-desc">details</p>
+        <div className="about-desc">
+        
+          <ul className="tech-list">
+            <li class="tech-list-item">
+                <span class="tooltip">React</span>
+                <span>
+                  <i class="fa-brands fa-react"></i>
+                </span>
+            </li>
+            <li class="tech-list-item">
+                <span class="tooltip">HTML</span>
+                <span>
+                  <i class="fa-brands fa-html5"></i>
+                </span>
+            </li>
+            <li class="tech-list-item">
+                <span class="tooltip">CSS / Sass</span>
+                <span>
+                <i class="fa-brands fa-css3-alt"></i>/<i class="fa-brands fa-sass"></i>
+                </span>
+            </li>
+            <li class="tech-list-item">
+                <span class="tooltip">JavaScript</span>
+                <span>
+                <i class="fa-brands fa-square-js"></i>
+                </span>
+            </li>
+            <li class="tech-list-item">
+                <span class="tooltip">Bootstrap</span>
+                <span>
+                <i class="fa-brands fa-bootstrap"></i>
+                </span>
+            </li>
+          </ul>
+
+          <ul className="about-socials">
+            <li className="about-social">
+              <a href="https://www.linkedin.com/in/jenniferstirpe/" target="_blank" aria-label="LinkedIn"><i className="fa-brands fa-linkedin"></i></a>
+            </li>
+            <li className="about-social">
+              <a href="https://github.com/jennstirpe" target="_blank" aria-label="GitHub"><i className="fa-brands fa-github-square"></i></a>
+            </li>
+          </ul>
+
+          <a class="about-resume" href={resume} target="_blank">Resume</a>
+
+        </div>
+
     </StyledAboutModal>
   )
 }
--- src/components/modals/styled/AboutModal.styled.jsx
+++ src/components/modals/styled/AboutModal.styled.jsx
@@ -6,6 +6,111 @@ export const StyledAboutModal = styled.section`
         padding: 1.25rem;
         margin: 1rem;
         line-height: 1.5rem;
+        height: 18rem;
         border: 1px solid ${({theme}) => theme.colors.accentDark}20;
+        border-radius: .25rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+
+        .about-resume {
+            text-decoration: none;
+            padding: .5rem 1rem;
+            border-radius: 1.5rem;
+            letter-spacing: 1px;
+            transition: all 150ms linear;
+            background: ${({theme}) => theme.colors.accentDark};
+            border: 1px solid ${({theme}) => theme.colors.textLight};
+            color: ${({theme}) => theme.colors.accentLight};
+
+            &:hover {
+                background: ${({theme}) => theme.colors.cardBg};
+                color: ${({theme}) => theme.colors.textDark};
+                border: 1px solid ${({theme}) => theme.colors.textDark};
+                box-shadow: 0 .25rem .25rem ${({theme}) => theme.colors.textDark}50;
+            }
+        }
+
+        .tech-list {
+            border: 1px solid ${({theme}) => theme.colors.accentDark}50;
+            width: 100%;
+            padding: .25rem 0;
+            border-radius: .5rem;
+            list-style: none;
+            display: flex;
+            justify-content: space-evenly;
+
+            .tech-list-item {
+                position: relative;
+                height: 3.5rem;
+                width: 3.5rem;
+                padding: .25rem;
+                border-radius: 50%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+                transition: all 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+            }
+
+            .tooltip {
+                position: absolute;
+                text-align: center;
+                top: 0;
+                font-size: .75rem;
+                background: #ffffff;
+                width: 5rem;
+                padding: .25rem .5rem;
+                border-radius: .5rem;
+                box-shadow: 0 .65rem .65rem rgba(0, 0, 0, 0.1);
+                opacity: 0;
+                pointer-events: none;
+                transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+            }
+
+            .tooltip::before {
+                position: absolute;
+                content: "";
+                height: .5rem;
+                width: .5rem;
+                background: #ffffff;
+                bottom: -.18rem;
+                left: 50%;
+                transform: translate(-50%) rotate(45deg);
+                transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+            }
+
+            .tech-list-item:hover .tooltip {
+                top: -1.75rem;
+                opacity: 1;
+                visibility: visible;
+                pointer-events: auto;
+            }
+
+            svg:hover span, svg:hover .tooltip {
+                text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
+            }
+        }
+
+        .about-socials {
+            width: 50%;
+            display: flex;
+            justify-content: space-evenly;
+            list-style: none;
+
+            .about-social {
+                font-size: 1.75rem;
+
+                a {
+                    color: ${({theme}) => theme.colors.accentDark};
+                    transition: all 150ms linear;
+                    
+                    &:hover {
+                        color: ${({theme}) => theme.colors.textLight};
+                    }
+                }  
+            }
+        }
     }
 `;
